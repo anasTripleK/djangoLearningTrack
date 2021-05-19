d98edc603a36061402d5d122b24d9d8733295847
anas11cs <annus.waheed01@gmail.com>
2021-05-19 23:15:52 +0500

    Made URLs Dynamic

--- ProjectZero/challenges/views.py
+++ ProjectZero/challenges/views.py
@@ -1,11 +1,20 @@
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hey, its January. Practice Urls ;)")
+def monthly_views(request, month):
 
+    textData = None
 
-def february(request):
-    return HttpResponse("Hey, its February. Practice Urls ;)")
+    if (month == "january"):
+        textData = "Hey, its January. Practice Urls ;)"
+    elif (month == "february"):
+        textData = "Hey, its February. Practice Urls ;)"
+    elif (month == "march"):
+        textData = "Hey, its March. Practice Urls ;)"
+    else:
+        return HttpResponseNotFound("Month Not Supported!")
+        a
+    return HttpResponse(textData)
