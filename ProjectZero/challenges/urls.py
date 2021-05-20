d36d30cddad154e619cc0a802bb0232637c26c49
anas11cs <annus.waheed01@gmail.com>
2021-05-20 06:17:17 +0500

    Dynamic Redirecting in Django

--- ProjectZero/challenges/urls.py
+++ ProjectZero/challenges/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("<int:month>", views.monthly_views_integer_override),
-    path("<str:month>", views.monthly_views),
+    path("<str:month>", views.monthly_views, name="monthsUrl"),
 ]
--- ProjectZero/challenges/views.py
+++ ProjectZero/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 months_catalog = {
@@ -21,23 +22,30 @@ months_catalog = {
 def monthly_views_integer_override(request, month):
     # Redirecting Techniques
 
-    # Redirect technique-1
     listOfMonths = list(months_catalog.keys())
-    if(month != 0):
-        targetValue = listOfMonths[month-1]
-        return monthly_views(request, targetValue)
-    else:
-        return HttpResponseNotFound("Integer: Invalid Month!")
+
+    # Redirect technique-1
+    # if(month != 0):
+    #     targetValue = listOfMonths[month-1]
+    #     return monthly_views(request, targetValue)
+    # else:
+    #     return HttpResponseNotFound("Integer: Invalid Month!")
 
     # Redirect technique-2
-    # try:
-    #     if(month != 0):
-    #         targetValue = listOfMonths[month-1]
-    #         return HttpResponseRedirect('/challenges/'+targetValue)
-    #     else:
-    #         return HttpResponseNotFound("Integer: Invalid Month!")
-    # except:
-    #     return HttpResponseNotFound("Integer: Invalid Month!")
+    try:
+        if(month != 0):
+            monthName = listOfMonths[month-1]
+
+            # Using reverse function will automatically track the url, if changed. Instead of applying pathChanges refactoring
+            # Dynamic Redirects
+            urlToRedirect = reverse("monthsUrl", args=[monthName])
+            # Constructed url will be /challenges/monthName
+
+            return HttpResponseRedirect(urlToRedirect)
+        else:
+            return HttpResponseNotFound("Integer: Invalid Month!")
+    except:
+        return HttpResponseNotFound("Integer: Invalid Month!")
 
 
 def monthly_views(request, month):
