ad8e88a87435c377c4786282ef2a161092351f4d
anas11cs <annus.waheed01@gmail.com>
2021-05-23 04:59:46 +0500

    DTL: Global Static Styles

--- ProjectZero/ProjectZero/settings.py
+++ ProjectZero/ProjectZero/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# To Tell django to check other static directories also
+STATICFILES_DIRS = [
+    BASE_DIR /  "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
