93f7518e6b0f866c2e3e6ca707960e1f013b7a89
anasTripleK <m.anas@triplek.tech>
2021-05-26 05:44:12 +0500

    SinglePost_Page(tempalte+css+view_config+url

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -6,5 +6,5 @@ def starting_page(request):
     return render(request, "blog/index.html")
 def all_posts(request):
     return render(request, "blog/all-posts.html")
-def each_post_detail(request):
-    pass
+def each_post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
